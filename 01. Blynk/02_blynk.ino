9f13264eb2c92e8cd3d38db5555edcc0bb638a91
lintangwisesa <lintangwisesa@ymail.com>
2018-10-13 12:55:31 +0700

    workshop iot dilo

---
+++ 01. Blynk/02_blynk.ino
@@ -0,0 +1,15 @@
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char token[] = "a6e801a2b49243e5a346235ff5c8a003";
+char ssidwifi[] = "DILO3";
+char passwifi[] = "DiloBSD123";
+
+void setup(){
+    Blynk.begin(token, ssidwifi, passwifi);
+}
+
+void loop(){
+    Blynk.run();
+}
---
+++ 01. Blynk/03_blynk_virtual.ino
@@ -0,0 +1,24 @@
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>  // ambil dari v0.4.6
+SimpleTimer timer;
+
+char token[] = "a6e801a2b49243e5a346235ff5c8a003";
+char ssidwifi[] = "DILO3";
+char passwifi[] = "DiloBSD123";
+
+void bacaDuaKaliPot(){
+    int duaPot = 2 * analogRead(A0);
+    Blynk.virtualWrite(V1, duaPot);
+}
+
+void setup(){
+    Blynk.begin(token, ssidwifi, passwifi);
+    timer.setInterval(1000L, bacaDuaKaliPot);
+}
+
+void loop(){
+    Blynk.run();
+    timer.run();
+}
